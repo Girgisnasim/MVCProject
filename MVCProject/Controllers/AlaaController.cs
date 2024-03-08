53d38e46f2135263ff5576f61ad57e9c92dfda4e
alaa alsayed <alaa alsayed@DESKTOP-FA5S6MN>
2024-03-08 22:31:48 +0200

    Alaa added controlle

---
+++ MVCProject/Controllers/AlaaController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MVCProject.Controllers
+{
+    public class AlaaController : Controller
+    {
+        public IActionResult Index()
+        {
+            return Content("WELCOME Alaa");
+        }
+    }
+}
