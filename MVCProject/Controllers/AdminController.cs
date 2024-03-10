223bdfc287a95ab0f1622db27d5780fd582c0acd
dell <dell@DESKTOP-9HC5TOG>
2024-03-10 21:45:01 +0200

    Trip operation

---
+++ MVCProject/Controllers/AdminController.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MVCProject.Models;
+using MVCProject.Repositories;
+
+namespace MVCProject.Controllers
+{
+    public class AdminController : Controller
+    {
+       private IAdminRepo AdminRepo;
+        public AdminController(IAdminRepo _adminRepo)
+        {
+            AdminRepo =_adminRepo;
+        }
+        public IActionResult Getall()
+        {
+            List<Trip> trips=AdminRepo.Getall();
+            return View(trips);
+        }
+        public IActionResult Add() {
+         return View();
+        }
+        [HttpPost]
+        public IActionResult Add(Trip trip)
+        {
+            AdminRepo.Add(trip);
+            return RedirectToAction("Getall");
+        }
+        public ActionResult update(int id)
+        {
+
+            Trip trip = AdminRepo.Get(id);
+            if (trip == null)
+            {
+                return NotFound();
+            }
+            return View(trip);
+
+        }
+        [HttpPost]
+        public ActionResult update(Trip trip) {
+
+            AdminRepo.Update(trip);
+            return RedirectToAction("Getall");
+
+        }
+        public ActionResult delete(int id)
+        {
+            AdminRepo.Delete(id);
+            return RedirectToAction("Getall");
+        }
+       
+    }
+}
---
+++ MVCProject/Migrations/20240310114400_v2.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace MVCProject.Migrations
+{
+    /// <inheritdoc />
+    public partial class v2 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Seats_Num",
+                table: "trips");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "image",
+                table: "trips",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "image",
+                table: "trips",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+
+            migrationBuilder.AddColumn<int>(
+                name: "Seats_Num",
+                table: "trips",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+        }
+    }
+}
